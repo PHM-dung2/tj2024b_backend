7d6221f8569e57a1c18080b6310ff7ec314fd893
PHM-dung2 <pp4992@naver.com>
2024-12-10 18:19:15 +0900

    java day2

--- tj2024b_backend/src/baekJoon/곱셈.java
+++ tj2024b_backend/src/baekJoon/곱셈.java
@@ -7,19 +7,22 @@ public class 곱셈 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int a, b, c, d , e;
+		int a, b , b1 , b2 , b3;
 		a = sc.nextInt();
 		b = sc.nextInt();
-		System.out.println("정수1 : ");
-		System.out.println("정수2 : ");
-		int result1 = a * b;
-		int result2 = a ;
-		int result3 = a ;
+		b1 = b / 100;
+		b2 = (b / 10) - b1 * 10 ;
+		b3 = b - (b1 * 100) - (b2 * 10);
+		
+		int result1 = a * b3;
+		int result2 = a * b2;
+		int result3 = a * b1;
+		int result4 = a * b;
 		
 		System.out.println( result1 );
 		System.out.println( result2 );
 		System.out.println( result3 );
-		System.out.println( result1 + (result1 * 10) + (result1 * 100) );
+		System.out.println( result4 );
 		
 	}
 
