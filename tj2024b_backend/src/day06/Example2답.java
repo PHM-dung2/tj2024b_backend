89a5e893fba321428485d29b0fdd5485e60cd804
PHM-dung2 <pp4992@naver.com>
2024-12-16 13:55:42 +0900

    day06 4교시

--- tj2024b_backend/src/day06/Example2답.java
+++ tj2024b_backend/src/day06/Example2답.java
@@ -16,8 +16,6 @@ import java.util.Scanner;
 		4. 선택시 : 현재 장바구니에 담긴 모든 현황을 보여주고 금액 합산을 출력한다.
 			ex) 콜라 1개 , 사이다 2개 , 환타 0개 총금액 : 4000원
 		* 추가 유효성검사 : 만일 재고가 부족하면 "재고가 부족합니다" 라고 출력하시오.
-		
-	
 */
 
 
@@ -33,18 +31,33 @@ public class Example2답 {
 		System.out.print(">>> 1.콜라 2.사이다 3.환타 4.결제 ");
 		int choose = sc.nextInt(); 
 	
-		if( choose == 1 ) {	--count1; }
-		
-		if( choose == 2 ) {	--count2; }
-		
-		if( choose == 3 ) {	--count3; }
-		
-		if( choose == 4 ) {
-			int price = (1000 * (10-count1)) + (1500 * (10-count2)) + (2000 * (10-count3));
-			System.out.printf("콜라 %d개 사이다 %d개 환타 %d개 총금액 : %d원\n" , 10-count1 , 10-count2 , 10-count3 , price );
+		if( choose == 1 ) {	System.out.println("콜라 선택 했습니다");
+			if( 콜라재고 == 0 ) { System.out.println(">> 콜라 재고가 부족합니다.");}
+			else {
+				콜라재고--;
+				콜라바구니++;	
+			}
+		}
+		else if( choose == 2 ) { System.out.println("사이다 선택 했습니다");
+			if( 사이다재고 == 0 ) { System.out.println(">> 사이다 재고가 부족합니다.");}
+			else {
+				사이다재고--;
+				사이다바구니++;	
+			}
+		}
+		else if( choose == 3 ) { System.out.println("환타 선택 했습니다");
+			if( 환타재고 == 0 ) { System.out.println(">> 환타 재고가 부족합니다.");}
+			else {
+				환타재고--;
+				환타바구니++;	
+			}
+		}
+		else if( choose == 4 ) {
+			int 총금액 = (콜라바구니*콜라가격) + (사이다바구니*사이다가격) + (환타바구니*환타가격);
+			System.out.printf("콜라 %2d개 사이다 %2d개 환타 %2d개 총금액 : %d원\n" , 콜라바구니 , 사이다바구니 , 환타바구니 , 총금액 );
+			콜라바구니 = 0; 사이다바구니 = 0; 환타바구니 = 0;
 		}
 	
-		
 	} // w e
 		
 		
