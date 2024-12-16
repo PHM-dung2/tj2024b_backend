26250361fe9d5c8dd6a4120430785d3a531deaf3
PHM-dung2 <pp4992@naver.com>
2024-12-16 15:14:35 +0900

    day06 5교시

---
+++ tj2024b_backend/src/day06/Example3.java
@@ -0,0 +1,24 @@
+package day06;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+
+		// 1분 복습 : 1.class 2.Person 3.int
+		// 생각해보기 : 생활속의 객체들을 클래스 코드로 표현하기
+			// - 학생,사람,자동차,주문 제외한다.
+			// - 작성후 카톡방에 제출
+		
+		/*
+		public class Book{
+			String 글쓴이;
+			String 제목;
+			String 출판사;
+			String 가격;
+		}
+		
+		*/
+		
+	}
+
+}
