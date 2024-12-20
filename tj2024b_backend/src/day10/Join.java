efd7d21bb505e968d2de3adb0e7ad5cccd6e459f
PHM-dung2 <pp4992@naver.com>
2024-12-20 15:44:28 +0900

    day10 6교시

---
+++ tj2024b_backend/src/day10/Join.java
@@ -0,0 +1,23 @@
+package day10;
+
+public class Join {
+	private String id;
+	private String pw;
+	private String nName;
+	
+	public Join() {};
+	public Join( String id , String pw , String writer) {
+		this.id = id;
+		this.pw = pw;
+		this.nName = nName;
+	} // m end
+	
+	public String getId() { return this.id; }
+	public void setId( String id ) { this.id = id; }	
+	public String getPw() { return this.pw; }
+	public void setPw( String pw ) { this.pw = pw; }	
+	public String getNName() { return this.nName; }
+	public void setNName( String nName ) { this.nName = nName; }	
+
+	
+}
