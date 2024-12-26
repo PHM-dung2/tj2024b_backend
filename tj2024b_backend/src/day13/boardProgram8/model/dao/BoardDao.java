f46b98b9d669ea75317d3f42a5b992b4fac29c24
PHM-dung2 <pp4992@naver.com>
2024-12-26 14:28:41 +0900

    day13 4교시

--- tj2024b_backend/src/day13/boardProgram8/model/dao/BoardDao.java
+++ tj2024b_backend/src/day13/boardProgram8/model/dao/BoardDao.java
@@ -1,5 +1,34 @@
 package day13.boardProgram8.model.dao;
 
+import day13.boardProgram8.model.dto.BoardDto;
+
 public class BoardDao {
 
+//	+ 싱글톤
+	private BoardDao() {}
+	private static BoardDao instance = new BoardDao();
+	public static BoardDao getInstance() {
+		return instance;
+	}
+//	- 싱글톤
+	
+//	+ 멤버변수( 모든 게시물정보를 갖는 배열 , 데이터베이스 열할 )
+	private BoardDto[] boardDB = new BoardDto[100];
+	
+//	1. 글쓰기 처리 메소드		, 함수명 : 임의 , 매개변수 : BoardDto , 리턴값 : true/false
+	public boolean write( BoardDto boardDto ) {
+//		[1] 매개변수로 받은 저장할 boardDto를 배열에 저장한다.
+		for( int index = 0 ; index < boardDB.length ; index++ ) {
+			if( boardDB[index] == null ) {
+				boardDB[index] = boardDto;
+				return true; // [2] 결과를 controller 응답한다.
+			}
+		}
+		return false; // [2] 결과를 controller 응답한다.
+	} // f end
+//	2. 모든 글출력 처리 메소드 , 함수명 : 임의 , 매개변수 : BoardDto , 리턴값 : true/false
+	public BoardDto[] findAll() {
+//		[1] 배열 내 모든 객체를 controller에게 응답한다.
+		return boardDB;
+	} // f end
 }
--- tj2024b_backend/src/day13/boardProgram8/view/BoardView.java
+++ tj2024b_backend/src/day13/boardProgram8/view/BoardView.java
@@ -2,6 +2,9 @@ package day13.boardProgram8.view;
 
 import java.util.Scanner;
 
+import day13.boardProgram8.controller.BoardController;
+import day13.boardProgram8.model.dto.BoardDto;
+
 public class BoardView {
 //	+ 싱글톤
 	private BoardView() {}
@@ -16,7 +19,6 @@ public class BoardView {
 	
 //	1. 메인페이지 입출력 메소드
 	public void index() {
-		
 		while(true) {
 			System.out.print(" [메인 페이지] 1.글등록 2.글출력 : ");
 			int choose = scan.nextInt();
@@ -27,11 +29,38 @@ public class BoardView {
 	} // f end
 //	2. 글쓰기 입출력 메소드		함수명 : 임의
 	public void write() {
+//		[1] 입력
 		System.out.println(" [글쓰기 페이지] ");
+		System.out.print(" 제목 : ");		String title = scan.next();
+		System.out.print(" 내용 : ");		String content = scan.next();
+		System.out.print(" 작성자 : ");	String writer = scan.next();
+//		[2] 객체화
+		BoardDto boardDto = new BoardDto(title, content, writer);
+//		[3] 컨트롤러에게 저장할 객체를 전달하고 결과를 응답받기
+		boolean result = BoardController.getInstance().write(boardDto);
+//		[4]
+		if( result == true ) {
+			System.out.println(" [글쓰기 성공] ");
+		}else {
+			System.out.println(" [글쓰기 실패] ");
+		}
 	}
 //	3. 모든 글출력 입출력 메소드	함수명 : 임의
 	public void findAll(){
 		System.out.println(" [모든글출력 페이지] ");
+//		[1] 컨트롤러에게 모든 게시물정보를 요청하고 결과받기
+		BoardDto[] result = BoardController.getInstance().findAll();
+//		[2] 요청 결과 출력하기
+		for( int index = 0 ; index < result.length ; index++ ) {
+			BoardDto board = result[index];
+			if( board != null ) {
+				System.out.printf(" %d , %s , %s , %s \n" , 
+						index + 1 , 
+						board.getTitle() , 
+						board.getContent() , 
+						board.getWriter() );
+			} // if end
+		}
 	}
 
 }
