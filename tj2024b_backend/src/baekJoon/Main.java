0c40f20b366a252fe2e9b73c87c47eee013e0462
PHM-dung2 <pp4992@naver.com>
2024-12-30 09:49:38 +0900

    -

--- tj2024b_backend/src/baekJoon/Main.java
+++ tj2024b_backend/src/baekJoon/Main.java
@@ -1,25 +1,9 @@
 package baekJoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedReader(new OutputStreamWriter(System.in));
-		StringTokenizer st;
-		int count = Integer.parseInt(br.readLine());
-		for( int i = 1 ; i <= count ; i++ ) {
-			st = new StringTokenizer(br.readLine());
-			bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
-		} // for end
+	public static void main(String[] args) {
 		
-		bw.close();
 	}
 	
 }
