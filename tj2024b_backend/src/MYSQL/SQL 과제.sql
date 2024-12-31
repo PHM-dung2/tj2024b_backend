94247a3dfd022f886798dcca333c7ecdbd99af41
PHM-dung2 <pp4992@naver.com>
2024-12-31 15:30:42 +0900

    sql

--- tj2024b_backend/src/MYSQL/SQL 과제.sql
+++ tj2024b_backend/src/MYSQL/SQL 과제.sql
@@ -14,34 +14,37 @@
 */
 create database kiosk;
 use kiosk;
+#주의할점은 테이블 생성시 상위PK 테이블 먼저 생성
+# [4] 카테고리 테이블
+create table categorie(
+	cno int auto_increment,				# 카테고리번호
+    kategorie varchar(30) not null,		# 카테고리명
+    constraint primary key( cno )		# pk 필드 설정
+);
+# [5] 제품 테이블
 create table product(
-	pno tinyint auto_increment,
-    price int not null,
-    pName varchar(30) not null unique,
-    constraint primary key( pno ),
-    kno tinyint,
-    constraint foreign key( kno ) references kategorie( kno )
+	pno int auto_increment,			# 제품번호
+    pname varchar(100) not null unique,	# 제품명
+    pprice int unsigned default 0,		# 제품가격
+    constraint primary key( pno ),		# pk 필드 설정
+    cno int,						# fk 필드 선언(pk필드와 동일한 타입 권장)
+    constraint foreign key( cno ) references categorie( cno ) # fk 필드 설정
+);
+# [6] 주문 테이블
+create table porder(
+	pono int auto_increment,			# 주문번호
+    pdate datetime default now(),		# 주문날짜 , 현재 시스템 날짜 자동
+    constraint primary key( pono )		# pk 필드 설정
 );
 
-create table kategorie(
-	kno int auto_increment,
-    kategorie varchar(30) not null unique,
-    constraint primary key( kno )
+create table porderdetail(
+	podno int auto_increment,				# 주문상세
+    podamount tinyint unsigned not null,	# 주문수량
+    constraint primary key( podno ),		# 주문상세 pk 설정
+    pno int,							# 제품번호 fk 설정
+    constraint foreign key( pno ) references product( pno ),
+    pono int,								# 주문번호 fk 설정
+    constraint foreign key( pono ) references porder( pono )
 );
-
-create table orderlist(
-	ono int auto_increment,
-    date time default now(),
-    constraint primary key( ono )
-);
-
-create table orderdetale(
-	dno int auto_increment,
-    constraint primary key( dno ),
-    ono int,
-    constraint foreign key( ono ) references orderlist( ono ),
-    kno tinyint,
-    constraint foreign key( kno ) references kategorie( kno ),
-    pno tinyint,
-    constraint foreign key( pno ) references product( pno )
-);
+# 테이블 간의 관계를 시작화적으로 표현 : ERD 다이어그램
+# 워크벤치에서 [datebase] - [reverse engineer]
