795621ce0198ef16ef4b809ae6bd9b16c657045a
PHM-dung2 <pp4992@naver.com>
2025-01-31 00:21:51 +0900

    백준 3052 문제풀이 3개

--- tj2024b_backend/src/baekJoon/b3052.java
+++ tj2024b_backend/src/baekJoon/b3052.java
@@ -3,29 +3,59 @@ package baekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class b3052 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] arr = new int[10];
+		
+//		방법 1
+		
+//		int[] arr = new int[10];
+//		
+//		for( int i = 0 ; i < 10 ; i++ ) {
+//			int a = Integer.parseInt(br.readLine());
+//			arr[i] = a % 42;
+//		} // for end
+//		
+//		int count = 0 ;
+//		boolean state = true;
+//		for( int i = 0 ; i < arr.length ; i++ ) {
+//			for( int j = 0 ; j < arr.length ; j++ ) {
+//				int a = arr[i];
+//				int b = arr[j];
+//				
+//				if( i != j && a == b ) { arr[i] = -1; }
+//			} // for end
+//			if( arr[i] >= 0 ) { count++; }
+//		} // for end
+//		System.out.println(count);
+		
+//		방법 2
+		
+//		HashSet<Integer> arr = new HashSet<Integer>();
+//		
+//		for( int i = 0 ; i < 10 ; i++ ) {
+//			arr.add( Integer.parseInt(br.readLine()) % 42 );
+//		} // for end
+//		
+//		System.out.println( arr.size() );
+//		
+//		br.close();
+		
+//		방법 3
+		boolean[] arr = new boolean[42];
 		
 		for( int i = 0 ; i < 10 ; i++ ) {
-			int a = Integer.parseInt(br.readLine());
-			arr[i] = a % 42;
+			arr[Integer.parseInt(br.readLine()) % 42] = true;
 		} // for end
 		
-		int count = 0 ;
-		boolean state = true;
-		for( int i = 0 ; i < arr.length ; i++ ) {
-			for( int j = 0 ; j < arr.length ; j++ ) {
-				int a = arr[i];
-				int b = arr[j];
-				
-				if( i != j && a == b ) { state = false; }
-			} // for end
-			if( state ) { count++; }
-		} // for end
-		System.out.println(count);
+		int count = 0;
+		for( boolean value : arr ) {
+			if( value ) { count++; }
+		}
+		
+		System.out.println( count );
 		
 	}
 }
