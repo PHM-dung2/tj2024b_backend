1d76c5432a0ee0ff0808ec0b7a2890bcdb7c3f25
PHM-dung2 <pp4992@naver.com>
2025-02-02 16:22:12 +0900

    백준 27866 문자와 문자열 문제풀이

---
+++ tj2024b_backend/src/baekJoon/b_27866.java
@@ -0,0 +1,23 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class b_27866 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String s = br.readLine();
+		int i = Integer.parseInt(br.readLine())-1;
+		
+		bw.write( s.charAt(i) );
+		
+		br.close();
+		bw.close();
+		
+	}
+}
