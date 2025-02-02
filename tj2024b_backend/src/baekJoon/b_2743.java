7a8b9d5a94fffedd2eba466fca0b9c821dbf39b4
PHM-dung2 <pp4992@naver.com>
2025-02-02 16:25:25 +0900

    백준 2743 단어 길이 재기 문제풀이

--- tj2024b_backend/src/baekJoon/b_2743.java
+++ tj2024b_backend/src/baekJoon/b_2743.java
@@ -1,5 +1,20 @@
 package baekJoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class b_2743 {
-	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str = br.readLine();
+		
+		bw.write( str.length()+"" );
+		
+		br.close();
+		bw.close();
+	}
 }
